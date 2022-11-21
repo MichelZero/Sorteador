b4604a19446cfacf3e69f04b41994b7bcad5e358
Michel <106674127+MichelZero@users.noreply.github.com>
2022-11-21 16:48:14 -0300

    up

---
+++ listanomes.py
@@ -0,0 +1,23 @@
+#
+# autores: 
+# Michel Silva
+#
+# criar uma lista de nomes
+# ler uma lista de nomes
+
+def listaNomes(tarefa):
+  """
+  Purpose: criar ou ler uma lista com nomes
+  """
+  if tarefa == "criar":
+    with open("nomes.txt", "w") as arquivo:
+      arquivo.write("Davi, Dani, Joao, Maria, Jose, Pedro, Ana, Paulo")
+  elif tarefa == "ler":
+    with open("nomes.txt", "r") as arquivo:
+      nomes = arquivo.read()
+      return nomes.split(", ")
+# end def
+  
+listaNomes("criar")
+lista01 = listaNomes("ler")
+print(lista01)
---
+++ sorteio-numero.py
@@ -0,0 +1,53 @@
+#
+# autores: 
+# Michel Silva
+#
+
+# sorteia um numero entre 1 e 100
+import random
+
+numero = random.randint(1, 100)
+print()
+print(f"O numero sorteado foi {numero}")
+
+
+# sorteia um nome de uma lista
+lista = ["Davi", "Dani", "Joao", "Maria", "Jose", "Pedro", "Ana", "Paulo"]
+
+nome = random.choice(lista)
+
+print(f"O nome sorteado foi {nome}")
+
+
+# criar uma função que sorteie um nome de uma lista
+def sortear(lista):
+    nome = random.choice(lista)
+    return nome
+  
+print(f"O nome sorteado foi {sortear(lista)}")
+
+
+# crie uma função para cara ou coroa  
+def cara_coroa():
+    opcao = random.choice(["cara", "coroa"])
+    return opcao
+  
+
+# criar uma função para jogar dados
+def dados():
+  """
+  Purpose: jogar dados
+  """
+  dado = random.randint(1, 6)
+  return dado
+
+
+# função corteio por um arquivo
+def sorteio_arquivo():
+  """
+  Purpose: sorteia um nome de um arquivo
+  """
+  arquivo = open("nomes.txt", "r")
+  nomes = arquivo.readlines()
+  arquivo.close()
+  return random.choice(nomes)
